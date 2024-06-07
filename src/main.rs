5e68b2d9219ed8e09b0395c89b082ada17768b01
Aurora <programming.aurora@gmail.com>
2024-06-07 16:36:36 +0100

    added model dependency

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use twilight_gateway::{Intents, Shard, ShardId};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	dotenv()?;
-	println!("Hello, world!");
 	let token = env::var("DISCORD_TOKEN")?;
 	let intents = Intents::GUILD_MESSAGES;
 	let mut shard = Shard::new(ShardId::ONE, token, intents);
 
+	println!("Starting shard");
 	while let Ok(event) = shard.next_event().await {
 		println!("{:?}", event);
 	}
