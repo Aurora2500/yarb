4f85fce02efd79b9b67d89522e38dce7689a9f81
Aurora <programming.aurora@gmail.com>
2024-06-07 16:24:02 +0100

    twilight setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+use twilight_gateway::{Intents, Shard, ShardId};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	dotenv()?;
+	println!("Hello, world!");
+	let token = env::var("DISCORD_TOKEN")?;
+	let intents = Intents::GUILD_MESSAGES;
+	let mut shard = Shard::new(ShardId::ONE, token, intents);
+
+	while let Ok(event) = shard.next_event().await {
+		println!("{:?}", event);
+	}
+
+	Ok(())
 }
